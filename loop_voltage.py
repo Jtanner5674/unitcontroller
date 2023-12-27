cc448e26f7aac0cc86d7fb4a5a2e9325810a69b6
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2023-12-27 14:27:45 -0500

    s

--- loop_voltage.py
+++ loop_voltage.py
@@ -13,19 +13,19 @@ try:
 except Exception as e:
     print("Error while initializing DAC:", e)
 
-voltage = 2 
+voltage = 2000 
 x = 1
 while x == 1:  # Correct the while loop syntax
     for _ in range(8):
-        voltage += 1
+        voltage += 1000
         dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL0)
         dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL1)
-        print(f"Voltage: {voltage/1000}")
+        print(f"Voltage: {voltage}")
         time.sleep(2)
 
     for _ in range(8):
-        voltage -= 1
+        voltage -= 1000
         dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL0) 
         dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL1)
-        print(f"Voltage: {voltage/1000}")
+        print(f"Voltage: {voltage}")
         time.sleep(2)
