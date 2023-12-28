29b145f90aa931c2bf3a9ec3626cbe60459bfd86
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2023-12-28 13:48:53 -0500

    updated code

--- loop_voltage.py
+++ loop_voltage.py
@@ -6,10 +6,9 @@ import time
 try:
     # Initialize the I2C bus
     i2c = busio.I2C(board.SCL, board.SDA)
-    dac = DFRobot_GP8403.DFRobot_GP8403(0x5e)  # Remove underscore in 0x_5e
+    dac = DFRobot_GP8403.DFRobot_GP8403(0x5e)
     dac.set_DAC_outrange(DFRobot_GP8403.OUTPUT_RANGE_10V)
-    dac.set_DAC_out_voltage(2, DFRobot_GP8403.CHANNEL0)
-    dac.set_DAC_out_voltage(2, DFRobot_GP8403.CHANNEL1)
+    dac.set_DAC_out_voltage(2, DFRobot_GP8403.CHANNEL3)
 except Exception as e:
     print("Error while initializing DAC:", e)
 
@@ -18,14 +17,12 @@ x = 1
 while x == 1:  # Correct the while loop syntax
     for _ in range(8):
         voltage += 1000
-        dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL0)
-        dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL1)
+        dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL3)
         print(f"Voltage: {voltage}")
         time.sleep(11)
 
     for _ in range(8):
         voltage -= 1000
-        dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL0) 
-        dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL1)
+        dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL3) 
         print(f"Voltage: {voltage}")
         time.sleep(11)
