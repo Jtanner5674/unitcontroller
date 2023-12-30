c208b3a8f513fb1c4cb1025a92b82a06456b7ce6
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2023-12-30 17:14:25 -0500

    s

--- loop_voltage.py
+++ loop_voltage.py
@@ -8,7 +8,7 @@ try:
     i2c = busio.I2C(board.SCL, board.SDA)
     dac = DFRobot_GP8403.DFRobot_GP8403(0x5e)
     dac.set_DAC_outrange(DFRobot_GP8403.OUTPUT_RANGE_10V)
-    dac.set_DAC_out_voltage(2, DFRobot_GP8403.CHANNEL3)
+    dac.set_DAC_out_voltage(2, DFRobot_GP8403.CHANNEL2)
 except Exception as e:
     print("Error while initializing DAC:", e)
 
@@ -17,12 +17,12 @@ x = 1
 while x == 1:  # Correct the while loop syntax
     for _ in range(8):
         voltage += 1000
-        dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL3)
+        dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL2)
         print(f"Voltage: {voltage}")
         time.sleep(11)
 
     for _ in range(8):
         voltage -= 1000
-        dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL3) 
+        dac.set_DAC_out_voltage(voltage, DFRobot_GP8403.CHANNEL2) 
         print(f"Voltage: {voltage}")
         time.sleep(11)
