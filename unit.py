0d31b87ff2d6913057dbce13c412b9fae41bddb0
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2023-12-30 17:32:47 -0500

    adjust voltage

--- unit.py
+++ unit.py
@@ -44,9 +44,9 @@ def set_voltage(dac_id):
     if dac:
         percentage = float(request.form['voltage1'])
         voltage = 2 + (percentage / 100) * 8
-        volts = voltage * 1000
+        volts = voltage * 500
         dac.set_DAC_out_voltage(volts, DFRobot_GP8403.CHANNEL0)
-        errval = f'Voltage set to {voltage}V for Channel 1 on address {hex(addr)} with id {dac_id}'
+        errval = f'Voltage set to {voltage}V for Channel 0 on address {hex(addr)} with id {dac_id}'
     else:
         errval = "Invalid DAC ID"
     print (errval)
@@ -57,11 +57,11 @@ def set_voltage2(dac_id):
     addr = dac_addresses.get(dac_id)
     dac = DFRobot_GP8403.DFRobot_GP8403(addr)
     if dac:
-        percentage = int(request.form['voltage1'])
+        percentage = int(request.form['voltage2'])
         voltage = 2 + (percentage / 100) * 8
-        volts = voltage * 1000
+        volts = voltage * 500
         dac.set_DAC_out_voltage(volts, DFRobot_GP8403.CHANNEL1)
-        errval = f'Voltage set to {voltage}V for Channel 2 on address {hex(addr)} with id {dac_id}'
+        errval = f'Voltage set to {voltage}V for Channel 1 on address {hex(addr)} with id {dac_id}'
     else:
         errval = "Invalid DAC ID"
     print (errval)
