ea0146bdf61307f55b8f151ad303609e49d81655
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2024-01-01 13:03:12 -0500

    d

--- unit.py
+++ unit.py
@@ -47,7 +47,7 @@ def index():
 
 @app.route('/settings')
 def settings():
-    return render_template('settings/index.html')
+    return render_template('settings/index.html', dac_addresses=dac_addresses)
 
 # Function Routes
 
