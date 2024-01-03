b60706b6bfbcb8bb464ed2cb7fef7b153cc19b5b
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2024-01-03 11:37:37 -0500

    j

--- unit.py
+++ unit.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify, request
 import busio
 import board
 import DFRobot_GP8403
+import json
 
 app = Flask(__name__)
 
 dac_objects = {}
 dac_addresses = {}
 
-        #Locate the DACS
 try:
     i2c = busio.I2C(board.SCL, board.SDA)
 
@@ -36,6 +36,11 @@ except Exception as e:
 @app.route('/')
 def index():
     return render_template('index.html', dac_objects=dac_objects)
+
+@app.route('/settings')
+def settings():
+    return render_template('settings/index.html')
+
 
 def set_voltage_action(dac_id, channel, value):
     addr = dac_addresses.get(dac_id)
@@ -72,6 +77,37 @@ def open1(dac_id):
 @app.route('/open2<int:dac_id>', methods=['POST'])
 def open2(dac_id):
     return set_voltage_action(dac_id, 2, 10000)
+
+# Function to load JSON data from a file
+def load_config():
+    with open('config.json', 'r') as file:
+        return json.load(file)
+
+# Function to save JSON data to a file
+def save_config(data):
+    with open('config.json', 'w') as file:
+        json.dump(data, file, indent=2)
+
+# Route to serve HTML form for updating configuration
+@app.route('/update_config/<string:section>/<int:index>', methods=['GET'])
+def update_config_form(section, index):
+    return render_template('update_config.html', section=section, index=index)
+
+# Route to get the entire configuration
+@app.route('/config', methods=['GET'])
+def get_config():
+    data = load_config()
+    return jsonify(data)
+
+# Route to update a specific configuration item
+@app.route('/config/<string:section>/<int:index>', methods=['PUT'])
+def update_config(section, index):
+    data = load_config()
+    new_value = request.json  # New value from the request
+    data[section][index] = new_value  # Update the specified item
+    save_config(data)  # Save updated data to the file
+    return jsonify(data[section][index])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
