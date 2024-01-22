f0d01fb5acf848313856fe64384e579849917268
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2024-01-22 01:10:41 -0500

    rename the config.jsons

--- unit.py
+++ unit.py
@@ -10,21 +10,21 @@ dac_objects = {}
 dac_addresses = {}
 
 def load_config():
-  with open('config_empty.json', 'r') as file:
+  with open('config.json', 'r') as file:
     return json.load(file)
 
 def save_config():
   T_CFG=CFG.copy()
   for key, value in T_CFG["dac"].items():
     value.pop('dac', None) # this will not crash if the element has no key 'dac'
-  with open('config_out.json', 'w') as file:
+  with open('config.json', 'w') as file:
     json.dump(T_CFG, file, indent=2)
 
 CFG=load_config()
 
 for item in CFG["dac"]:
     item["found"] = False
-    
+
 print(CFG)
 
 print("Scanning I2C bus for DACs...")
