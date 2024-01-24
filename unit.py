6a5a63f007d9bd2d6b75ad92d316f2c9643591f9
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2024-01-24 18:51:39 -0500

    initalize before access

--- unit.py
+++ unit.py
@@ -115,10 +115,12 @@ def close1(addr):
 def open1(addr):
     return set_voltage_action(addr, 10000)
 
-@app.route('/config', methods=['GET'])
 def get_dac_config():
+    initialize_dacs()
+    
     existing_configs = load_config()
     return jsonify({'dac_addresses': CFG["dac"], 'existing_configs': existing_configs["dac"]})
+
 
 # Route to serve HTML form for updating configuration
 @app.route('/update_config/<string:section>/<int:index>', methods=['GET'])
