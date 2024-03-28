de76231bcf703be575f848e316cf49faec0bc05c
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2024-03-28 10:19:16 -0400

    save both?

--- unit.py
+++ unit.py
@@ -22,14 +22,11 @@ def load_config():
 
 def save_config(config):
     config_to_save = {
-        "dac": [
-            {key: value for key, value in dac.items() if key != "obj"}
-            for dac in config["dac"]
-        ],
-        "presets": config.get("presets", {})
+        key: value for key, value in config.items() if key != "obj"
     }
     with open('config.json', 'w') as file:
         json.dump(config_to_save, file, indent=2)
+
 
 
 def set_voltage_action(addr, percent):
