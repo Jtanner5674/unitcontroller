76802fe8e260c5b15fd28d540de1d2c97ce0c6dd
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2024-04-19 15:39:48 -0400

    f

--- RelayController.py
+++ RelayController.py
@@ -4,35 +4,31 @@ class RelayController:
     def __init__(self, address, bus_number=1):
         self.bus = smbus.SMBus(bus_number)
         self.address = address
-        self.current_status = 0b00000000  # Assume all relays are initially off
+        self.current_status = 0b00000000  # Start with all relays off
 
     def _send_update(self):
-        self.bus.write_byte(self.address, self.current_status)
+        # Make sure current_status is always a byte when sending to hardware
+        self.bus.write_byte(self.address, self.current_status & 0xFF)
 
     def on(self, *relays):
-        if not relays:  # If no relay number is provided, turn all on
-            self.current_status = 0b11111111
+        if not relays:  # Turn all relays on if no specific relay is provided
+            self.current_status = 0xFF
         else:
             for relay in relays:
                 self.current_status |= (1 << (relay - 1))
         self._send_update()
 
     def off(self, *relays):
-        if not relays:  # If no relay number is provided, turn all off
-            self.current_status = 0b00000000
+        if not relays:  # Turn all relays off if no specific relay is provided
+            self.current_status = 0x00
         else:
             for relay in relays:
                 self.current_status &= ~(1 << (relay - 1))
         self._send_update()
 
-
 # Example usage:
-# Create an instance of the RelayController
-#relay = RelayController(bus_number=1, address=0x27)
-
-# Interface methods
-#relay.on(1)     # Turn on relay 1
-#relay.off(3)    # Turn off relay 3
-#relay.on(1, 4)  # Turn on relay 1 and 4
-#relay.off()     # Turn off all relays
-#relay.on()      # Turn on all relays
+# relay_controller = RelayController(address=0x27)
+# relay_controller.on(1)  # Turns on relay 1
+# relay_controller.off(2)  # Turns off relay 2
+# relay_controller.on()    # Turns all relays on
+# relay_controller.off()   # Turns all relays off
