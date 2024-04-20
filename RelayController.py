0d232be22ccb1e951695299ac9fc73d9c5d5079f
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2024-04-20 00:27:59 -0400

    s

--- RelayController.py
+++ RelayController.py
@@ -4,30 +4,28 @@ class RelayController:
     def __init__(self, address, bus_number=1):
         self.bus = smbus.SMBus(bus_number)
         self.address = address
-        self.current_status = 0b11111111  # Start with all relays off (inverted logic)
+        self.current_status = 0b11111111  # Start with all relays off
 
     def _send_update(self):
-        # This method sends the full byte of current_status to the I2C address
-        formatted_status = '0b' + format(self.current_status, '08b')
-        print(f"Sending update to hardware: {formatted_status}")
+        # Directly send the current_status byte to the hardware
         self.bus.write_byte(self.address, self.current_status)
 
     def on(self, *relays):
-        # Turns specified relays on (0 = ON)
+        # Turns specified relays on by setting corresponding bits to 0
         if not relays:
             self.current_status = 0b00000000  # All relays on
         else:
             for relay in relays:
-                self.current_status &= ~(1 << (relay - 1))  # Clear bit to turn on
+                self.current_status &= ~(1 << (relay - 1))
         self._send_update()
 
     def off(self, *relays):
-        # Turns specified relays off (1 = OFF)
+        # Turns specified relays off by setting corresponding bits to 1
         if not relays:
             self.current_status = 0b11111111  # All relays off
         else:
             for relay in relays:
-                self.current_status |= (1 << (relay - 1))  # Set bit to turn off
+                self.current_status |= (1 << (relay - 1))
         self._send_update()
 
 # Example usage
