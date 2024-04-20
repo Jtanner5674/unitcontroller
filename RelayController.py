7661d57b902f8ee538ac2822087c20331ba6a257
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2024-04-20 00:11:26 -0400

    s

--- RelayController.py
+++ RelayController.py
@@ -4,27 +4,28 @@ class RelayController:
     def __init__(self, address, bus_number=1):
         self.bus = smbus.SMBus(bus_number)
         self.address = address
-        self.current_status = 0b00000000  # Start with all relays off
+        self.current_status = 0xFF  # Start with all relays off (inverted logic)
 
     def _send_update(self):
-        print(f"Sending update to hardware: {self.current_status:08b}")
+        # Inverted logic: 1 is off, 0 is on
         self.bus.write_byte(self.address, self.current_status & 0xFF)
 
     def on(self, *relays):
         if not relays:  # Turn all relays on if no specific relay is provided
+            self.current_status = 0x00
+        else:
+            for relay in relays:
+                self.current_status &= ~(1 << (relay - 1))
+        self._send_update()
+
+    def off(self, *relays):
+        if not relays:  # Turn all relays off if no specific relay is provided
             self.current_status = 0xFF
         else:
             for relay in relays:
                 self.current_status |= (1 << (relay - 1))
         self._send_update()
 
-    def off(self, *relays):
-        if not relays:  # Turn all relays off if no specific relay is provided
-            self.current_status = 0x00
-        else:
-            for relay in relays:
-                self.current_status &= ~(1 << (relay - 1))
-        self._send_update()
 
 # Example usage:
 # relay_controller = RelayController(address=0x27)
