87d73e404cd4edb42712425816850986bb4499d7
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2024-04-20 00:31:44 -0400

    l

--- RelayController.py
+++ RelayController.py
@@ -4,27 +4,32 @@ class RelayController:
     def __init__(self, address, bus_number=1):
         self.bus = smbus.SMBus(bus_number)
         self.address = address
-        self.current_status = 0b11111111  # Start with all relays off
+        # Start with all relays off (1 = OFF, 0 = ON)
+        self.current_status = 0b11111111
 
     def _send_update(self):
-        # Directly send the current_status byte to the hardware
+        # Send the full byte to the hardware
         self.bus.write_byte(self.address, self.current_status)
 
     def on(self, *relays):
         # Turns specified relays on by setting corresponding bits to 0
         if not relays:
-            self.current_status = 0b00000000  # All relays on
+            # If no relay numbers are provided, turn all relays on
+            self.current_status = 0b00000000
         else:
             for relay in relays:
+                # Bitwise AND with the complement of the bit mask to turn on relay
                 self.current_status &= ~(1 << (relay - 1))
         self._send_update()
 
     def off(self, *relays):
         # Turns specified relays off by setting corresponding bits to 1
         if not relays:
-            self.current_status = 0b11111111  # All relays off
+            # If no relay numbers are provided, turn all relays off
+            self.current_status = 0b11111111
         else:
             for relay in relays:
+                # Bitwise OR with the bit mask to turn off relay
                 self.current_status |= (1 << (relay - 1))
         self._send_update()
 
