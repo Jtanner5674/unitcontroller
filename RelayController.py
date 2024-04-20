67dea68c2e7f524c2a7ffab1292fcd5bc03e4f5f
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2024-04-20 03:19:53 -0400

    s

--- RelayController.py
+++ RelayController.py
@@ -4,39 +4,43 @@ class RelayController(PCF8574):
     def __init__(self, address, bus_number=1):
         super().__init__(address, bus_number)
         self.state = 0xFF  # All relays are off initially (0b11111111)
+        self.write(self.state)  # Ensure initial state is set
 
     def on(self, *relays):
-        # If no relay numbers are given, turn all relays on
         if not relays:
-            self.state = 0x00
+            # Turn the first four relays on (set the first four bits to 0)
+            self.state &= 0x0F
         else:
             for relay in relays:
                 if 1 <= relay <= 4:
-                    self.state &= ~(1 << (relay - 1))
+                    # Set the bit corresponding to the relay to 0
+                    self.state &= ~(0x80 >> (relay - 1))
                 else:
                     raise ValueError("Relay number must be between 1 and 4")
         self.write(self.state)
 
     def off(self, *relays):
-        # If no relay numbers are given, turn all relays off
         if not relays:
-            self.state = 0xFF
+            # Turn the first four relays off (set the first four bits to 1)
+            self.state |= 0xF0
         else:
             for relay in relays:
                 if 1 <= relay <= 4:
-                    self.state |= (1 << (relay - 1))
+                    # Set the bit corresponding to the relay to 1
+                    self.state |= (0x80 >> (relay - 1))
                 else:
                     raise ValueError("Relay number must be between 1 and 4")
         self.write(self.state)
 
     def toggle(self, relay):
-        """Toggle the specified relay."""
         if 1 <= relay <= 4:
-            self.state ^= (1 << (relay - 1))
+            # Toggle the bit corresponding to the relay
+            self.state ^= (0x80 >> (relay - 1))
             self.write(self.state)
         else:
             raise ValueError("Relay number must be between 1 and 4")
 
     def get_state(self):
-        """Return the current state of all relays."""
-        return bin(self.state)
+        # Return the current state of all relays with the first four bits
+        return bin(self.state & 0xF0)
+
