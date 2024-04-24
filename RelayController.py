443d666e2736addc19d0de4801ca3191f5130a98
Jtanner5674 <79053412+Jtanner5674@users.noreply.github.com>
2024-04-24 09:33:58 -0400

    s

--- RelayController.py
+++ RelayController.py
@@ -1,4 +1,5 @@
 from PCF8574 import PCF8574
+from time import sleep
 
 class RelayController(PCF8574):
     def __init__(self, address, bus_number=1):
@@ -8,12 +9,10 @@ class RelayController(PCF8574):
 
     def on(self, *relays):
         if not relays:
-            # Turn the first four relays on (set the first four bits to 0)
             self.state &= 0x0F
         else:
             for relay in relays:
                 if 1 <= relay <= 4:
-                    # Set the bit corresponding to the relay to 0
                     self.state &= ~(0x80 >> (relay - 1))
                 else:
                     raise ValueError("Relay number must be between 1 and 4")
@@ -21,12 +20,10 @@ class RelayController(PCF8574):
 
     def off(self, *relays):
         if not relays:
-            # Turn the first four relays off (set the first four bits to 1)
             self.state |= 0xF0
         else:
             for relay in relays:
                 if 1 <= relay <= 4:
-                    # Set the bit corresponding to the relay to 1
                     self.state |= (0x80 >> (relay - 1))
                 else:
                     raise ValueError("Relay number must be between 1 and 4")
@@ -34,13 +31,18 @@ class RelayController(PCF8574):
 
     def toggle(self, relay):
         if 1 <= relay <= 4:
-            # Toggle the bit corresponding to the relay
             self.state ^= (0x80 >> (relay - 1))
             self.write(self.state)
         else:
             raise ValueError("Relay number must be between 1 and 4")
 
     def get_state(self):
-        # Return the current state of all relays with the first four bits
         return bin(self.state & 0xF0)
 
+def enginestarter(self, live, starter):
+    self.on(live, starter) 
+    sleep(2)  # Ensure there is a delay for the starter to engage properly
+    self.off(starter)
+    self.state = self.get_state()
+    self.state = int(self.state, 2) + live  # Convert the binary string to an integer before addition
+    self.write(self.state) 
